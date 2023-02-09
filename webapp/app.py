a7dc55bd8be0121ebccb3811a9dccece287c2a69
Vaibhav Shukla <vaibhavshukla1967@gmail.com>
2023-02-09 23:07:37 +0530

    Created a web app with streamlit to display the recommendations

--- webapp/app.py
+++ webapp/app.py
@@ -1,12 +1,18 @@
+import os
+from dotenv import load_dotenv
+
 import streamlit as st
 import pickle
 import pandas as pd
 import requests
 
+load_dotenv()
+api = os.getenv("API_KEY")
+
 
 def fetch_poster(movie_id):
     response = requests.get(
-        'https://api.themoviedb.org/3/movie/{}?api_key=afc2cbfb7493ca32a38e8e5f220f4f8e&language=en-US'.format(movie_id)
+        f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={api}&language=en-US"
     )
     data = response.json()
     return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
@@ -39,6 +45,8 @@ selected_movie_name = st.selectbox(
     movies['title'].values)
 
 if st.button('Show recommendations'):
+    st.text(f"Recommendations for {selected_movie_name}")
+    st.image(fetch_poster(movies[movies['title'] == selected_movie_name].movie_id.values[0]))
     names, posters = recommend(selected_movie_name)
     col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
